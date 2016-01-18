e955c50b7d8e87ec7e3b68d651e770604f0d7e0d
krecikos87 <krecikos87@gmail.com>
2016-01-18 16:43:38 +0100

    Added selenium test taken from selenium IDE

--- src/test/java/phoneValidatorTest.java
+++ src/test/java/phoneValidatorTest.java
@@ -9,14 +9,21 @@ import static org.junit.Assert.*;
 public class phoneValidatorTest {
     @Test
     @Parameters({"1234567890, true",
+                "12345678901, false",
+                "123456781, false",
                 "123-456-7890, true",
                 "123-456-7890 x1234, true",
                 "123-456-7890 ext1234, true",
                 "(123)-456-7890, true",
                 "123.456.7890, true",
-                "123 456 7890, true"
-
+                "123.456..7890, false",
+                "123..456.7890, false",
+                "123 456 7890, true",
+                "123456 7890, false",
+                "123 4567890, false"
     })
+
+
     public void shouldPrintSmthWeWant1(String number, boolean valid){
         boolean result = PhoneValidator.validatePhoneNumber(number);
         assertEquals(result, valid);
