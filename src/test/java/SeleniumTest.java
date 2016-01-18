1c8545477a15967e4b63b94aa368ff56d7381fb3
Marcin <suchan.mb@gmail.com>
2016-01-18 16:46:35 +0100

    Adding junit depandancy

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,77 @@
+/**
+ * Created by Administrator on 2016-01-18.
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+    public class SeleniumTest {
+        private WebDriver driver;
+        private String baseUrl;
+        private boolean acceptNextAlert = true;
+        private StringBuffer verificationErrors = new StringBuffer();
+
+        @Before
+        public void setUp() throws Exception {
+            driver = new FirefoxDriver();
+            baseUrl = "http://agileszkolenia.pl/";
+        }
+
+        @Test
+        public void test() throws Exception {
+            driver.get(baseUrl + "/");
+            driver.findElement(By.linkText("Szkolenia")).click();
+            driver.findElement(By.id("mt-dla-organizacji")).click();
+            assertEquals("Leading SAFe", driver.findElement(By.linkText("Leading SAFe")).getText());
+        }
+
+        @After
+        public void tearDown() throws Exception {
+            driver.quit();
+            String verificationErrorString = verificationErrors.toString();
+            if (!"".equals(verificationErrorString)) {
+                fail(verificationErrorString);
+            }
+        }
+
+        private boolean isElementPresent(By by) {
+            try {
+                driver.findElement(by);
+                return true;
+            } catch (NoSuchElementException e) {
+                return false;
+            }
+        }
+
+        private boolean isAlertPresent() {
+            try {
+                driver.switchTo().alert();
+                return true;
+            } catch (NoAlertPresentException e) {
+                return false;
+            }
+        }
+
+        private String closeAlertAndGetItsText() {
+            try {
+                Alert alert = driver.switchTo().alert();
+                String alertText = alert.getText();
+                if (acceptNextAlert) {
+                    alert.accept();
+                } else {
+                    alert.dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+
+
