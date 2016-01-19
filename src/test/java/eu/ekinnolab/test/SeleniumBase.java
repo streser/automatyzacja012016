56e09b5e94ed25e69fdaea4df474c917763e8d21
kamil.tor <kamil.tor@ekinnolab.eu>
2016-01-19 10:19:05 +0100

    after refactoring

---
+++ src/test/java/eu/ekinnolab/test/SeleniumBase.java
@@ -0,0 +1,80 @@
+package eu.ekinnolab.test;
+
+import static org.junit.Assert.fail;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Kamil Tor on 19 sty 2016
+ */
+public class SeleniumBase {
+	WebDriver driver;
+	protected String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	public SeleniumBase() {
+		super();
+	}
+	
+	@BeforeClass
+	public static void beforeScenario() throws Exception {
+		System.out.println("beforeScen parent");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "http://agileszkolenia.pl/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		System.out.println("in parent");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+}
---
+++ src/test/java/eu/ekinnolab/test/WrittenSeleniumTest.java
@@ -0,0 +1,47 @@
+package eu.ekinnolab.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class WrittenSeleniumTest extends SeleniumBase {
+
+	public void givenIamOnMainPage() {
+		driver.get(baseUrl + "/");
+	}
+
+	@Test
+	public void testSeleniumIDE() throws Exception {
+		givenIamOnMainPage();
+		whenIGoToCoachingSection();
+		thenIshouldSeeAgileCoachingOffer();
+	}
+
+	@Test
+	public void testSeleniumManually() throws Exception {
+		givenIamOnMainPage();
+		whenIgoToOrganizationSection();
+		thenIshouldSeeSafeWorkshop();
+	}
+
+	public void thenIshouldSeeAgileCoachingOffer() {
+		assertEquals("Agile Coaching", driver.findElement(By.cssSelector("h1.entry-title")).getText());
+	}
+
+	public void thenIshouldSeeSafeWorkshop() {
+		driver.findElement(By.linkText("Leading SAFe"));
+	}
+
+	public void whenIGoToCoachingSection() {
+		driver.findElement(By.linkText("Konsulting")).click();
+		driver.findElement(By.linkText("Szkolenia")).click();
+		driver.findElement(By.id("mt-dla-testerow")).click();
+		driver.findElement(By.linkText("Coaching")).click();
+	}
+
+	public void whenIgoToOrganizationSection() {
+		driver.findElement(By.id("menu-item-181")).click();
+		driver.findElement(By.id("mt-dla-organizacji")).click();
+	}
+}
