ec697a602abba950e428637d08a4e63bf77a7b0c
Marcin <suchan.mb@gmail.com>
2016-01-19 10:13:46 +0100

    Adding junit depandancy

--- src/test/java/Configuration.java
+++ src/test/java/Configuration.java
@@ -1,7 +1,10 @@
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Administrator on 2016-01-19.
@@ -19,5 +22,26 @@ public class Configuration {
     @After
     public void tearDown() throws Exception {
         driver.quit();
+    }
+
+
+    public void OpenUrl(String url)
+    {
+        driver.get(url);
+    }
+
+    public void OpenLink(String linkToOpen)
+    {
+        driver.findElement(By.linkText(linkToOpen)).click();
+    }
+
+    public void FindElement(String id)
+    {
+        driver.findElement(By.id(id)).click();
+    }
+
+    public void AssertThatTextEqualsValue(String textToCheck)
+    {
+        assertEquals(textToCheck, driver.findElement(By.linkText(textToCheck)).getText());
     }
 }
--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -3,9 +3,6 @@
  */
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-
-import static org.junit.Assert.assertEquals;
 
     public class SeleniumTest extends Configuration {
 
@@ -17,26 +14,6 @@ import static org.junit.Assert.assertEquals;
             FindElement("mt-dla-organizacji");
             AssertThatTextEqualsValue("Leading SAFe");
 
-        }
-
-        public void OpenUrl(String url)
-        {
-            driver.get(url);
-        }
-
-        public void OpenLink(String linkToOpen)
-        {
-            driver.findElement(By.linkText(linkToOpen)).click();
-        }
-
-        public void FindElement(String id)
-        {
-            driver.findElement(By.id(id)).click();
-        }
-
-        public void AssertThatTextEqualsValue(String textToCheck)
-        {
-            assertEquals(textToCheck, driver.findElement(By.linkText(textToCheck)).getText());
         }
 
     }
