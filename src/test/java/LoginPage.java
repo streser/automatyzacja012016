62517f7df6d076b8565261152fbbe0b81e658414
Marcin <suchan.mb@gmail.com>
2016-01-19 12:46:39 +0100

    Adding junit depandancy

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Administrator on 2016-01-19.
  */
@@ -20,6 +22,11 @@ public class LoginPage {
         driver.findElement(By.id("user_login")).sendKeys(login);
         driver.findElement(By.id("user_pass")).sendKeys(password);
         driver.findElement(By.id("wp-submit")).click();
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return new MainPage(driver);
 
     }
